cf6cea7ae73c9f789b5004bb87e23eaddc87e628
Freitas-Manu <freitasmanuela.caetano@gmail.com>
2026-05-11 10:49:05 -0300

    inicio css e simulados

---
+++ front-clube-do-livro/src/components/Simulados/Simulados.jsx
@@ -0,0 +1,12 @@
+import Inicio from './components/Inicio/Inicio';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Inicio />
+        </div>
+    );
+}
+
+export default App;
