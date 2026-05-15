a3d309db6a37ecbfe20888cd32edd3cce3a26fd9
jennifer <jennifer.k.silva7@aluno.senai.br>
2026-05-15 14:54:14 -0300

    feat: estrutura paginas de forma correta e cria configuração inicial do aquivo App.jsx

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+
+function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>LOGO</div>
+            <ul className={styles.links}>
+                <li>
+                    {' '}
+                    <Link to="/">Home</Link>{' '}
+                </li>
+                <li>
+                    {' '}
+                    <Link to="/sobre">Sobre</Link>{' '}
+                </li>
+                <li>
+                    {' '}
+                    <Link to="servicos">Serviços</Link>{' '}
+                </li>
+                <li>
+                    {' '}
+                    <Link to="/contato">Contato</Link>{' '}
+                </li>
+                <li>
+                    {' '}
+                    <Link to="/Extra">Blog</Link>{' '}
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
