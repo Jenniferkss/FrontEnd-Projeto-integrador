9345f3cd0dd035b06d4c8df01843de3245767415
jennifer <jennifer.k.silva7@aluno.senai.br>
2026-05-18 11:03:59 -0300

    feat: finaliza pagina inicial de curiosidades e adiciona footer ao projeto

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css';
+const Footer = () => {
+    return (
+     <footer className={styles.footerBottom}>
+                2026 - Página dedicada à obra 'Quarto de Despejo: Diário de Uma Favelada' de Carolina Maria de Jesus. Todos os direitos reservados.
+            </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Curiosidades/Curiosidades.jsx
+++ src/pages/Curiosidades/Curiosidades.jsx
@@ -1,7 +1,8 @@
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 import styles from './Curiosidades.module.css';
 
-const Obra = () => {
+const Curiosidades = () => {
     return (
         <div className={styles.pageContainer}>
             <div className={styles.boxedLayout}>
@@ -9,14 +10,40 @@ const Obra = () => {
 
                 <main className={styles.hero}>
                     <h1 className={styles.title}>
-                        {' '}
-                        Bem-Vindos ao <br />
-                        <span className={styles.italicTitle}>Intertexto!</span>
+                        {'Curiosidades'} <br />
+                        <span className={styles.italicTitle}>clique e conheça:</span>
                     </h1>
+                    <div className={styles.cardsContainer}>
+
+                        <div className={styles.cardsContainer}>
+
+                            {}
+                            <div className={styles.card}>
+                                <div className={styles.imageWrapper}>
+                                    <img src="/images/curiosidade1.png" alt="Vida da autora" />
+                                </div>
+                                <button link="/vida-da-autora">
+                                    Vida da autora
+                                </button>
+                            </div>
+
+                            {}
+                            <div className={styles.card}>
+                                <div className={styles.imageWrapper}>
+                                    <img src="/images/curiosidade2.png" alt="Contexto histórico" />
+                                </div>
+                                <button link="/contexto-historico">
+                                    Contexto histórico
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
                 </main>
+                <Footer />
             </div>
         </div>
     );
 };
 
-export default Obra;
+export default Curiosidades;
