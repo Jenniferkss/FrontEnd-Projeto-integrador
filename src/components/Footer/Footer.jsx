401b94dcbc84ae53a0c7849aeb9c5a802bcbab30
Jenifer <jennifer.k.silva7@aluno.senai.br>
2026-05-18 14:37:14 -0300

    feat:adiciona paginas de contexto historico e vida da autora dentro de curiosidades

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import styles from './Footer.module.css';
 const Footer = () => {
     return (
-     <footer className={styles.footerBottom}>
-                2026 - Página dedicada à obra 'Quarto de Despejo: Diário de Uma Favelada' de Carolina Maria de Jesus. Todos os direitos reservados.
-            </footer>
+        <footer className={styles.footerBottom}>
+            &copy; {new Date().getFullYear()} - Página dedicada à obra 'Quarto de Despejo: Diário de
+            Uma Favelada' de Carolina Maria de Jesus. Todos os direitos reservados.
+        </footer>
     );
 };
 
