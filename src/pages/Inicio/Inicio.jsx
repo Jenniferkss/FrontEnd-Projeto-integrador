9d6e17f6bc3d4df64cb7b9ef00f1480736a5eddc
maria.c.ferreira47@aluno.senai.br <mlkliso4000@gmail.com>
2026-05-18 09:31:22 -0300

    Feat: Ajusta o código da pasta inicio

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -11,18 +11,17 @@ const Inicio = () => {
                     <h1 className={styles.title}>
                         {' '}
                         Bem-Vindos ao <br />
-                        <span className={styles.italicTitle}>Intertexto!</span>
+                        <h3 className={styles.italicTitle}>Intertexto!</h3>
                     </h1>
 
                     <p className={styles.description}>
                         Neste site você terá acesso à análise das obras clássicas brasileiras,
                         principalmente à obra
-                        <span className={styles.highlight}>
-                            {''} Quarto de Despejo: Diário de Uma Favelada
-                        </span>
-                        , de
-                        <span className={styles.highlight}> Carolina Maria de Jesus</span>
+                        <p className={styles.highlight}>
+                            Quarto de Despejo: Diário de Uma Favelada, de Carolina Maria de Jesus </p>
                     </p>
+             
+                       
 
                     <div className={styles.btnGroup}>
                         <button className={styles.btn}>Conheça Quarto de Despejo</button>
