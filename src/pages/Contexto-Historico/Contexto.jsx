9ebb613e780d617db7bda7c3f21e8ed0b48af028
jennifer <jennifer.k.silva7@aluno.senai.br>
2026-05-25 09:44:27 -0300

    feat: adiciona estilização inicial na pagina contexto historico

--- src/pages/Contexto-Historico/Contexto.jsx
+++ src/pages/Contexto-Historico/Contexto.jsx
@@ -1,6 +1,7 @@
 import Header from '../../components/Header/Header';
 import styles from './Contexto.module.css';
 import Footer from '../../components/Footer/Footer';
+import Card from '../../components/Card/Card';
 
 const Contexto = () => {
     return (
@@ -11,10 +12,25 @@ const Contexto = () => {
                 <main className={styles.hero}>
                     <h2>Contexto Historico (1955-1960) </h2>
                     <p>Texto vai aqui</p>
-                </main>
+                    <section className={styles.cardsContainer}>
+                        <Card
+                            titulo="O Brasil de JK"
+                            descricao="Industrialização"
+                        />
 
+                        <Card
+                            titulo="Favela de Canindé"
+                            descricao="Pobreza urbana"
+                        />
+
+                        <Card
+                            titulo="Impacto da obra"
+                            descricao="Best seller mundial"
+                        />
+                    </section>
+                </main>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
